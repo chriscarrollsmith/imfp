61727324f847b43d0914a2628abe95e09b891677
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-03-25 11:48:44 -0400

    Built some backend utility functions

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-TEST
+imf_rate_limit = RateLimiter(max_calls=5, period=5)
--- scripts/utils.py
+++ scripts/utils.py
@@ -3,23 +3,51 @@ import time
 import requests
 import json
 import functools
+import pandas as pd
 from ratelimiter import RateLimiter
 from pkg_resources import get_distribution
 
-def rate_limited(rate_limit):
+def _imf_rate_limited(imf_rate_limit):
+    """
+    (Internal) Decorator function for rate limiting the decorated function.
+
+    Args:
+        rate_limit (RateLimiter): A RateLimiter object specifying the maximum number of calls and the time period.
+
+    Returns:
+        Callable: A decorated function that is rate-limited according to the provided RateLimiter object.
+    """
+    
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            with rate_limit:
+            with imf_rate_limit:
                 return func(*args, **kwargs)
         return wrapper
     return decorator
 
-rate_limit = RateLimiter(max_calls=5, period=5)
+
+imf_rate_limit = RateLimiter(max_calls=5, period=5)
 
 
-@rate_limited(rate_limit)
-def download_parse(URL, times=3):
+@_imf_rate_limited(rate_limit)
+def _imf_download_parse(URL, times=3):
+    """
+    (Internal) Download and parse JSON content from a URL with rate limiting and retries.
+
+    This function is rate-limited and will perform a specified number of retries in case of failure.
+
+    Args:
+        URL (str): The URL to download and parse the JSON content from.
+        times (int, optional): The number of times to retry the request in case of failure. Defaults to 3.
+
+    Returns:
+        dict: The parsed JSON content as a Python dictionary.
+
+    Raises:
+        ValueError: If the content cannot be parsed as JSON after the specified number of retries.
+    """
+    
     app_name = os.environ.get("IMF_APP_NAME")
     if app_name:
         app_name = app_name[:255]
@@ -48,3 +76,34 @@ def download_parse(URL, times=3):
                 time.sleep(2 ** (_ + 1))
             else:
                 raise
+
+
+def _imf_dimensions(database_id, times=3, inputs_only=True):
+    """
+    (Internal) Retrieve the list of codes for dimensions of an individual IMF database.
+
+    Args:
+        database_id (str): The ID of the IMF database.
+        times (int, optional): The number of times to retry the request in case of failure. Defaults to 3.
+        inputs_only (bool, optional): If True, only include input parameters. Defaults to True.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the parameter names and their corresponding codes and descriptions.
+    """
+    URL = f'http://dataservices.imf.org/REST/SDMX_JSON.svc/DataStructure/{database_id}'
+    raw_dl = _download_parse(URL, times)
+
+    code = raw_dl['Structure']['CodeLists']['CodeList']['@id']
+    description = raw_dl['Structure']['CodeLists']['CodeList']['Name']['#text']
+    codelist_df = pd.DataFrame({'code': [code], 'description': [description]})
+
+    params = [dim['@conceptRef'].lower() for dim in raw_dl['Structure']['KeyFamilies']['KeyFamily']['Components']['Dimension']]
+    codes = [dim['@codelist'] for dim in raw_dl['Structure']['KeyFamilies']['KeyFamily']['Components']['Dimension']]
+    param_code_df = pd.DataFrame({'parameter': params, 'code': codes})
+
+    if inputs_only:
+        result_df = param_code_df.merge(codelist_df, on='code', how='left')
+    else:
+        result_df = param_code_df.merge(codelist_df, on='code', how='outer')
+
+    return result_df
