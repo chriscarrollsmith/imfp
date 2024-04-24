a9414fb1de3cd06a10a9d2314b0f63e0755ab04f
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2024-04-24 09:38:07 -0400

    Run black with poetry in Github Actions workflow

--- README_files/render.py
+++ README_files/render.py
@@ -3,17 +3,20 @@ import re
 import nbconvert
 import nbformat
 
+
 def remove_scoped_styles(markdown_content):
-    style_pattern = re.compile(r'<style scoped>.*?</style>', re.DOTALL)
-    return style_pattern.sub('', markdown_content)
+    style_pattern = re.compile(r"<style scoped>.*?</style>", re.DOTALL)
+    return style_pattern.sub("", markdown_content)
+
 
 def replace_png_links(markdown_content):
-    png_pattern = re.compile(r'!\[png\]\(.*?\.png\)', re.DOTALL)
-    return png_pattern.sub('![png](README_files/plot.png)', markdown_content)
+    png_pattern = re.compile(r"!\[png\]\(.*?\.png\)", re.DOTALL)
+    return png_pattern.sub("![png](README_files/plot.png)", markdown_content)
+
 
 def convert_notebook_to_markdown(input_file, output_file):
     # Read the notebook
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(input_file, "r", encoding="utf-8") as f:
         nb_content = f.read()
 
     # Parse the notebook content
@@ -30,13 +33,14 @@ def convert_notebook_to_markdown(input_file, output_file):
     markdown_output = replace_png_links(markdown_output)
 
     # Write the result to the output file
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write(markdown_output)
+
 
 if __name__ == "__main__":
     script_path = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(script_path)
-    
+
     input_file = os.path.join(project_root, "README.ipynb")
     output_file = os.path.join(project_root, "README.md")
 
